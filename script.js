b10d70c24760c00f5a75fbbf9d07d1b4590dc76b
yeohwaihong <yeohwaihong@gmail.com>
2017-05-14 15:44:03 +0800

    change website lol

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
+$(document).ready(function(){
+  jQuery('.statistic-counter').counterUp({
+    delay: 10,
+    time: 2000
+});
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
 
-// counter box
-// function modify_qty(val) {
-//     var qty = document.getElementById('qty').value;
-//     var new_qty = parseInt(qty,10) + val;
-//
-//     if (new_qty < 0) {
-//         new_qty = 0;
-//     }
-//
-//     document.getElementById('qty').value = new_qty;
-//     return new_qty;
-// }
-// var slideIndex = 0;
-// showSlides();
-//
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex> slides.length) {slideIndex = 1}
-//     slides[slideIndex-1].style.display = "block";
-//     setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-var slideIndex = 1;
-showSlides(slideIndex);
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    // Store hash
+    var hash = this.hash;
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
